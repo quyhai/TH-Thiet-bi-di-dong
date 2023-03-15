660fad259072897a30ef25ef1b93fe4fe5c087ed
quyhai <117881805+quyhai@users.noreply.github.com>
2023-03-15 13:12:08 +0700

    Update cau1.dart

--- Tuan5/cau1.dart
+++ Tuan5/cau1.dart
@@ -2,7 +2,7 @@ import 'dart:io';
 
 void main() {
   File file = File('hello.txt');
-  String Name = 'Nguyễn Thanh Bình';
+  String Name = 'Nguyễn Quý Hải';
   file.writeAsStringSync('Hello ${Name}\n', mode: FileMode.append);
   print('Thêm tên thành công!');
 }
